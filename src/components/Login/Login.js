6868ef8a406ca585d96effff0bce769e4a24d2c6
LaZyRaifur <raifurrahimcse13@gmail.com>
2021-10-23 18:27:51 +0600

    using mongodb and nodejs

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,12 +1,49 @@
-import React, { useContext } from 'react';
-
+import React, { useContext } from "react";
+import firebase from "firebase/app";
+import "firebase/auth";
+import firebaseConfig from "./firebase.config";
+import { createContext } from "react";
+import { UserContext } from "../../App";
+import { useHistory, useLocation } from "react-router-dom";
 
 const Login = () => {
-    return (
-        <div>
-            <h1>This is Login</h1>
-        </div>
-    );
+
+
+    const [loggedInUser,setLoggedInUser] = useContext(UserContext);
+    const history = useHistory();
+    const location = useLocation();
+
+    const { from } = location.state || { from: { pathname: "/" } };
+    if(firebase.apps.length === 0){
+        firebase.initializeApp(firebaseConfig);
+    }
+    
+  const handleGoogleSignIn = () => {
+    var provider = new firebase.auth.GoogleAuthProvider();
+    firebase
+      .auth()
+      .signInWithPopup(provider)
+      .then((result) => {
+       
+        const {displayName,email} = result.user;
+        const signInUser = {name:displayName,email:email}
+        setLoggedInUser(signInUser);
+        history.replace(from);
+        // ...
+      })
+      .catch((error) => {
+       
+        var errorMessage = error.message;
+        console.log(errorMessage);
+       
+      });
+  };
+  return (
+    <div>
+      <h1>This is Login</h1>
+      <button onClick={handleGoogleSignIn}>Google Sign In</button>
+    </div>
+  );
 };
 
 export default Login;
---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCjlxHRCh6vbRlYKp9MKiM62ryrMBxEZBU",
+    authDomain: "burj-al-arab-b9537.firebaseapp.com",
+    projectId: "burj-al-arab-b9537",
+    storageBucket: "burj-al-arab-b9537.appspot.com",
+    messagingSenderId: "892997825576",
+    appId: "1:892997825576:web:5ccde192d3ed997160b11d"
+  };
+  export default firebaseConfig;
